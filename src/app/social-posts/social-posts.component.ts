e9324bd0864d55759b8307f98a6237efa9ed5061
Alana Frank <alanamarielle@gmail.com>
2020-11-10 19:05:14 -0500

    wip

---
+++ src/app/social-posts/social-posts.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../interfaces/post';
+
+@Component({
+  selector: 'app-social-posts',
+  templateUrl: './social-posts.component.html',
+  styleUrls: ['./social-posts.component.css'],
+})
+export class SocialPostsComponent implements OnInit {
+  posts: Post[] = [
+    {
+      title: "Alana's First Post",
+      thought: "Let's see if this thing works!",
+    },
+    {
+      title: "Alana's Second Post",
+      thought: 'It works, but does it REALLY work?',
+    },
+    {
+      title: 'Unicorns',
+      thought: 'They are real.',
+    },
+    {
+      title: 'Elevators',
+      thought: 'Going Up!',
+    },
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  deletePost = (index: number): void => {
+    this.posts.splice(index, 1);
+  };
+  addPost = (post: Post) => {
+    this.posts.push(post);
+  };
+}
